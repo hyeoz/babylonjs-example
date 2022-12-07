0aa963c41e86fba75d255382349021c1c2fa39c2
hyeoz <jenny970203@gmail.com>
2022-12-07 17:09:00 +0900

    chap.1 큐브 회전 애니메이션

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 const canvas = document.getElementById("renderCanvas"); // 캔버스 엘리먼트 찾음
 const engine = new BABYLON.Engine(canvas, true); // BABYLON 3D engine 생성
 
+var getYSpin = function (animationSpeed) {
+  const frameRate = 1.0 / animationSpeed;
+  var ySpin = new BABYLON.Animation(
+    "ySpin",
+    "rotation",
+    1,
+    BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
+    BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE
+  );
+  var keyFrames = [];
+  keyFrames.push({
+    frame: 0,
+    value: BABYLON.Vector3.Zero(),
+  });
+  keyFrames.push({
+    frame: frameRate,
+    value: new BABYLON.Vector3(-Math.PI, Math.PI, Math.PI),
+  });
+  keyFrames.push({
+    frame: 2 * frameRate,
+    value: new BABYLON.Vector3(Math.PI, Math.PI, -Math.PI),
+  });
+  ySpin.setKeys(keyFrames);
+  return ySpin;
+};
+
 const createScene = function () {
   const scene = new BABYLON.Scene(engine); // 장면 생성. 엔진을 인수로 넘겨줌
 
@@ -22,7 +48,10 @@ const createScene = function () {
   ); // 조명 생성. 반구형 조명으로 name, direction, scene 울 매개변수로 받음
 
   const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene); // 상자 생성. 중심이 원점에 위치하도록 생성되며 name, option, scene 세가지의 매개변수를 받음
-
+  const animationSpeed = 1;
+  var ySpin = getYSpin(animationSpeed);
+  box.animations.push(ySpin);
+  scene.beginDirectAnimation(box, [ySpin], 0, 10, true);
   return scene;
 };
 
